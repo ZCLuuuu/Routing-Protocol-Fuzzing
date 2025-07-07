cd918f631c3003da7ece9cac1bd532000e42d7ee
ZCLuuuu <zhanglv0413@163.com>
2025-07-07 20:12:41 +1000

    Update BGPFuzz dependency installation: Tshark.

--- Fuzz-Material/set_env.sh
+++ Fuzz-Material/set_env.sh
@@ -1,5 +1,5 @@
 source ../GNS3-Ubuntu-Setup/venv4gns3/bin/activate
-sudo apt-get install graphviz libgraphviz-dev graphviz-dev pkg-config expect
+sudo apt-get install tshark graphviz libgraphviz-dev graphviz-dev pkg-config expect
 pip install pygraphviz
 pip install fuzzingbook
 pip install pyshark
